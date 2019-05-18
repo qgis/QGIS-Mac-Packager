d13dd17dda6a3b4200c1e3054f63a239ea6a0ce5
Peter Petrik <zilolv@gmail.com>
2019-05-18 14:38:30 +0200

    push versions

--- scripts/run_ltr.bash
+++ scripts/run_ltr.bash
@@ -5,7 +5,7 @@ set -e
 # 2018 Peter Petrik (zilolv at gmail dot com)
 # GNU General Public License 2 any later version
 
-TAG=final-3_4_7
+TAG=final-3_4_8
 QGISAPP="QGIS3.4.app"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BD=$DIR/../../builds/ltr
--- scripts/run_pr.bash
+++ scripts/run_pr.bash
@@ -5,7 +5,7 @@ set -e
 # 2018 Peter Petrik (zilolv at gmail dot com)
 # GNU General Public License 2 any later version
 
-TAG=final-3_6_2
+TAG=final-3_6_3
 QGISAPP="QGIS3.6.app"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BD=$DIR/../../builds/pr
