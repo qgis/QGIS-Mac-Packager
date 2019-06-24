b7a7b50e15989993b0ea3f3445fde49ae17638ad
Peter Petrik <zilolv@gmail.com>
2019-06-24 07:37:43 +0200

    ver++

--- scripts/run_pr.bash
+++ scripts/run_pr.bash
@@ -5,8 +5,8 @@ set -e
 # 2018 Peter Petrik (zilolv at gmail dot com)
 # GNU General Public License 2 any later version
 
-TAG=final-3_6_3
-QGISAPP="QGIS3.6.app"
+TAG=final-3_8_0
+QGISAPP="QGIS3.8.app"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BD=$DIR/../../builds/pr
 
