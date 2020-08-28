ba2569a91ccb13f1ca963ea6e637d1f7b6fff2eb
Peter Petrik <zilolv@gmail.com>
2020-08-28 11:39:52 +0200

    fix #77, pillow import

--- qgis_bundle/recipes/python_patsy/recipe.sh
+++ qgis_bundle/recipes/python_patsy/recipe.sh
@@ -14,25 +14,11 @@ function bundle_python_patsy() {
 }
 
 function fix_binaries_python_patsy() {
-  patsy_EGG=$BUNDLE_PYTHON_SITE_PACKAGES_DIR/patsy-${VERSION_python_patsy}-py${VERSION_major_python}-macosx-${MACOSX_DEPLOYMENT_TARGET}-x86_64.egg
-
-  for i in \
-    _list \
-    _datadir \
-    _crs \
-    _geod \
-    _proj \
-    _transformer
-  do
-    :
-    # install_name_change $DEPS_LIB_DIR/$LINK_libproj @rpath/$LINK_libproj $patsy_EGG/patsy/$i.cpython-${VERSION_major_python//./}m-darwin.so
-  done
+  :
 }
 
 function fix_binaries_python_patsy_check() {
-  patsy_EGG=$BUNDLE_PYTHON_SITE_PACKAGES_DIR/patsy-${VERSION_python_patsy}-py${VERSION_major_python}-macosx-${MACOSX_DEPLOYMENT_TARGET}-x86_64.egg
-
-  # verify_binary $patsy_EGG/patsy/_proj.cpython-${VERSION_major_python//./}m-darwin.so
+  :
 }
 
 function fix_paths_python_patsy() {
--- qgis_bundle/recipes/python_pillow/recipe.sh
+++ qgis_bundle/recipes/python_pillow/recipe.sh
@@ -17,22 +17,45 @@ function fix_binaries_python_pillow() {
   pillow_EGG=$BUNDLE_PYTHON_SITE_PACKAGES_DIR/pillow-${VERSION_python_pillow}-py${VERSION_major_python}-macosx-${MACOSX_DEPLOYMENT_TARGET}-x86_64.egg
 
   for i in \
-    _list \
-    _datadir \
-    _crs \
-    _geod \
-    _proj \
-    _transformer
+    _imaging.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingcms.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingft.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingmath.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingmorph.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingtk.cpython-${VERSION_major_python//./}m-darwin.so \
+    _webp.cpython-${VERSION_major_python//./}m-darwin.so
   do
-    :
-    # install_name_change $DEPS_LIB_DIR/$LINK_libproj @rpath/$LINK_libproj $pillow_EGG/pillow/$i.cpython-${VERSION_major_python//./}m-darwin.so
+    install_name_id @rpath/$i $pillow_EGG/PIL/$i
   done
+
+  for j in \
+      $LINK_jpeg \
+      $LINK_openjpeg \
+      $LINK_zlib \
+      $LINK_libtiff
+  do
+    install_name_change $DEPS_LIB_DIR/$j @rpath/$j $pillow_EGG/PIL/_imaging.cpython-${VERSION_major_python//./}m-darwin.so
+  done
+
+  install_name_change $DEPS_LIB_DIR/$LINK_little_cms2 @rpath/$LINK_little_cms2 $pillow_EGG/PIL/_imagingcms.cpython-${VERSION_major_python//./}m-darwin.so
+  install_name_change $DEPS_LIB_DIR/$LINK_freetype @rpath/$LINK_freetype $pillow_EGG/PIL/_imagingft.cpython-${VERSION_major_python//./}m-darwin.so
+  install_name_change $DEPS_LIB_DIR/$LINK_libwebp @rpath/$LINK_libwebp $pillow_EGG/PIL/_webp.cpython-${VERSION_major_python//./}m-darwin.so
 }
 
 function fix_binaries_python_pillow_check() {
   pillow_EGG=$BUNDLE_PYTHON_SITE_PACKAGES_DIR/pillow-${VERSION_python_pillow}-py${VERSION_major_python}-macosx-${MACOSX_DEPLOYMENT_TARGET}-x86_64.egg
 
-  # verify_binary $pillow_EGG/pillow/_proj.cpython-${VERSION_major_python//./}m-darwin.so
+  for i in \
+    _imaging.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingcms.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingft.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingmath.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingmorph.cpython-${VERSION_major_python//./}m-darwin.so \
+    _imagingtk.cpython-${VERSION_major_python//./}m-darwin.so \
+    _webp.cpython-${VERSION_major_python//./}m-darwin.so
+  do
+    verify_binary $pillow_EGG/PIL/$i
+  done
 }
 
 function fix_paths_python_pillow() {
