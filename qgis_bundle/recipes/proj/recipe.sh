cd7a11f7d774e51d60e9e7da262ee1e5a9ded4c6
Peter Petrik <zilolv@gmail.com>
2020-09-03 19:35:57 +0200

    fix #47 package proj grids

--- qgis_bundle/recipes/proj/recipe.sh
+++ qgis_bundle/recipes/proj/recipe.sh
@@ -12,6 +12,13 @@ function bundle_proj() {
 
   # see src/app/main.cpp for PROJ_LIB setup for bundle
   try rsync -av $DEPS_SHARE_DIR/proj $BUNDLE_RESOURCES_DIR/
+
+  # https://github.com/qgis/QGIS-Mac-Packager/issues/47
+  GRID_DIR=$QGIS_BUNDLE_SCRIPT_DIR/../../proj-datumgrid/grids
+  if [ ! -d "$GRID_DIR" ]; then
+    error "Missing $GRID_DIR. Use scripts/fetch_proj-datumgrid.bash to download grids"
+  fi
+  try cp -av $GRID_DIR/* $BUNDLE_RESOURCES_DIR/proj/
 }
 
 function fix_binaries_proj() {
