f35f3d9805aecd98b95ed3a532237b0ae7d36b55
Peter Petrik <zilolv@gmail.com>
2021-01-27 13:45:07 +0100

    fix name

--- qgis_bundle/recipes/qgis/recipe.sh
+++ qgis_bundle/recipes/qgis/recipe.sh
@@ -288,10 +288,14 @@ function fix_binaries_qgis_check() {
 function fix_paths_qgis() {
  ###################
  # Patch Info.plist
+ QGIS_APP_NAME_SHORT=${QGIS_APP_NAME/.app/}
  /usr/libexec/PlistBuddy -c "Add :LSMinimumSystemVersion string $MACOSX_DEPLOYMENT_TARGET" $BUNDLE_CONTENTS_DIR/Info.plist
  /usr/libexec/PlistBuddy -c "Add :LSFileQuarantineEnabled bool false" $BUNDLE_CONTENTS_DIR/Info.plist
  # PYQGIS_STARTUP should be relative to Contents/Resources/python
  /usr/libexec/PlistBuddy -c "Add :LSEnvironment:PYQGIS_STARTUP string pyqgis-startup.py" $BUNDLE_CONTENTS_DIR/Info.plist
+ /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier string org.qgis.qgis$QGIS_MAJOR_VERSION_$QGIS_MINOR_VERSION" $BUNDLE_CONTENTS_DIR/Info.plist
+ /usr/libexec/PlistBuddy -c "Set :CFBundleName string $QGIS_APP_NAME_SHORT" $BUNDLE_CONTENTS_DIR/Info.plist
+ /usr/libexec/PlistBuddy -c "Set :CFBundleSignature string $QGIS_APP_NAME_SHORT" $BUNDLE_CONTENTS_DIR/Info.plist
  # PROJ_LIB see ../proj/recipe.sh
  # GDAL_DATA and GDAL_DRIVER_PATH see ../gdal/recipe.sh
  # PYTHONHOME see ../python/recipe.sh
