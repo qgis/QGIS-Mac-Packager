9519da6a0345bf0c25cead0891e87636b5e8b6f0
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-02-11 15:58:32 +0100

    disable java

--- qgis_deps/recipes/gettext/recipe.sh
+++ qgis_deps/recipes/gettext/recipe.sh
@@ -52,6 +52,7 @@ function build_gettext() {
   try ${CONFIGURE} \
     --disable-debug \
     --disable-static \
+    --disable-java \
     --program-prefix=g
 
   check_file_configuration config.status
