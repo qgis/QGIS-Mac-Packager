ec1f885f26d7f79afcff7e8dc3daf6797fd6c49f
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-02-18 11:26:37 +0100

    fixes

--- qgis_deps/build-deps.sh
+++ qgis_deps/build-deps.sh
@@ -2,6 +2,5 @@
 
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export DEPS_BUILD_PATH=${CUR_DIR}/../build/deps
 export SOURCE_PACKAGES_PATH=${CUR_DIR}/../build/.packages
 ./distribute.sh dev -mqgis_deps
