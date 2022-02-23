2bc7e111663fc583d7c426d494f1aa85bcd7662b
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-02-23 07:04:36 +0100

    o

--- qgis_deps/recipes/spatialite/build.sh
+++ qgis_deps/recipes/spatialite/build.sh
@@ -2,13 +2,13 @@ function build_spatialite() {
   try cd ${DEPS_BUILD_PATH}/spatialite/build-$ARCH
   push_env
 
+  export CFLAGS="$CFLAGS -O2 -DPIC"
+
   try ${CONFIGURE} \
     --enable-shared=yes \
     --enable-static=no \
     --enable-geocallbacks \
     --disable-dependency-tracking
-
-  export CFLAGS="$CFLAGS -O2 -DPIC"
 
   check_file_configuration config.status
   try $MAKESMP
