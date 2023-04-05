862da6079e70235f78ac149ad6d2de9b6eae0700
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-04-05 15:17:24 +0900

    bump openssl 1.1.x to 3.1.0

--- qgis_deps/recipes/openssl/recipe.sh
+++ qgis_deps/recipes/openssl/recipe.sh
@@ -5,20 +5,20 @@ DESC_openssl="Cryptography and SSL/TLS Toolkit"
 # version of your package
 # NOTE openssl version must be compatible with QT version, for example
 # for Qt 5.14 see https://wiki.qt.io/Qt_5.14.1_Known_Issues
-VERSION_openssl=1.1.1h
+VERSION_openssl=3.1.0
 
 # dependencies of this recipe
 DEPS_openssl=()
 
-LINK_libssl_version=1.1
+LINK_libssl_version=3
 LINK_libssl=libssl.${LINK_libssl_version}.dylib
 LINK_libcrypto=libcrypto.${LINK_libssl_version}.dylib
 
 # url of the package
-URL_openssl=https://github.com/openssl/openssl/archive/OpenSSL_${VERSION_openssl//./_}.tar.gz
+URL_openssl=https://github.com/openssl/openssl/releases/download/openssl-${VERSION_openssl}/openssl-${VERSION_openssl}.tar.gz
 
 # md5 of the package
-MD5_openssl=e365330dabdc29bb8b5d2984f14d2e62
+MD5_openssl=f6c520aa2206d4d1fa71ea30b5e9a56d
 
 # default build path
 BUILD_openssl=$BUILD_PATH/openssl/$(get_directory $URL_openssl)
@@ -64,7 +64,7 @@ function build_openssl() {
   try perl ./Configure \
     --prefix=$STAGE_PATH \
     --openssldir=$STAGE_PATH \
-    darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 \
+    darwin64-`uname -m`-cc enable-ec_nistp_64_gcc_128 \
     no-ssl3 \
     no-ssl3-method \
     no-zlib \
