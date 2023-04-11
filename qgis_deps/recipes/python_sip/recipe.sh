1a831c13547a323141842156e941b005498f297a
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-04-11 20:32:41 +0900

    added pyqt_builder, remove comment out in sip

--- qgis_deps/recipes/python_sip/recipe.sh
+++ qgis_deps/recipes/python_sip/recipe.sh
@@ -50,21 +50,6 @@ function build_python_sip() {
 
   push_env
 
-  # try $PYTHON ./configure.py \
-  #   --sipdir=$STAGE_PATH/share/sip \
-  #   --bindir=$STAGE_PATH/bin \
-  #   --deployment-target=$MACOSX_DEPLOYMENT_TARGET \
-  #   --destdir=$QGIS_SITE_PACKAGES_PATH \
-  #   --incdir=$STAGE_PATH/include \
-  #   --sip-module=PyQt5.sip
-
-  # try $MAKESMP
-  # try $MAKE install
-  # try $MAKE clean
-
-
-  # default directory for sip files
-  # mkdir -p ${STAGE_PATH}/share/sip
   try $PYTHON setup.py install
 
   pop_env
