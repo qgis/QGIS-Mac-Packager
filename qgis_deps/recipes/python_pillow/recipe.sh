25d85b3ae7cf8d28db37a986cffc7ed9130898ee
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-04-27 15:56:33 +0900

    current pillow is not need manually patch

--- qgis_deps/recipes/python_pillow/recipe.sh
+++ qgis_deps/recipes/python_pillow/recipe.sh
@@ -46,14 +46,6 @@ function build_python_pillow() {
   try cd $BUILD_PATH/python_pillow/build-$ARCH
   push_env
 
-  # unfortunately they named the file Zip.h, clashing with the lzip library
-  # see https://github.com/python-pillow/Pillow/pull/4812
-  try mv src/libImaging/Zip.h src/libImaging/Zip2.h
-  try ${SED} "s;Zip.h;Zip2.h;g" src/libImaging/ZipDecode.c
-  try ${SED} "s;Zip.h;Zip2.h;g" src/libImaging/ZipEncode.c
-  try ${SED} "s;Zip.h;Zip2.h;g" src/decode.c
-  try ${SED} "s;Zip.h;Zip2.h;g" src/encode.c
-
   DYLD_LIBRARY_PATH=$STAGE_PATH/lib try $PYTHON setup.py install
 
   pop_env
